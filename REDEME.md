52d3ceed81dea681a960d44485558b230eb60662
zhangxuekun <zhangxuekun@wtoip.com>
2019-12-10 12:26:38 +0800

    feat:update dockerfile

--- REDEME.md
+++ REDEME.md
@@ -1,4 +1,8 @@
 ```
- docker image build --build-arg node_env=dev -t hello-docker:1.0.2 .
+ docker  build --build-arg node_env=dev -t hello-docker:1.0.0 .
  docker run -p 8090:30010 -d hello-docker
+```
+
+```
+ carefull : linux server zone time
 ```
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const http = require('http');
 const PORT = 30010;
 
 const server = http.createServer((req,res)=>{
-   res.end('Hello Docker');
+  let date = new Date();
+   res.end(`Hello Docker ,time:${date.toLocaleString()}`);
 })
 
 server.listen(PORT,()=>{
-    console.log(`Running on http://localhost:${PORT}`);
+    console.log(`Running on http://localhost:${PORT}, NODE_ENV=${ process.env.NODE_ENV}`,);
 });
